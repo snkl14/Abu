b0047f28d359ebc08ff8b0b4643fe80c291c6732
SHEFIN KL14 <93778980+shefinkl14@users.noreply.github.com>
2022-04-07 20:24:45 +0530

    Update genQr.js

--- plugins/genQr.js
+++ plugins/genQr.js
@@ -8,11 +8,11 @@ if (Config.WORKTYPE == 'private') {
 
 addCommand({pattern: 'gqr ?(.*)', fromMe: true, desc: "Text to qr." }, (async (m, match) => {
 
-    if (match[1] === '') return await m.sendMessage("Enter a text \n\nExample .gqr hi");
+    if (match[1] === '') return await message.sendMessage("Enter a text \n\nExample .gqr hi");
 
     var sn = await axios.get(`https://early-pie-production.up.railway.app/genqr?text=${match[1].replace(/#/g, '\n')} `, { responseType: 'arraybuffer' })
 
-    await m.sendMessage(Buffer.from(sn.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.ALL})
+    await message.sendMessage(Buffer.from(sn.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.ALL})
 }));
 }
 
@@ -20,10 +20,10 @@ if (Config.WORKTYPE == 'public') {
 
 addCommand({pattern: 'gqr ?(.*)', fromMe: true, desc: "Text to qr." }, (async (m, match) => {
 
-    if (match[1] === '') return await m.sendMessage("Enter a text \n\nExample .gqr hi");
+    if (match[1] === '') return await message.sendMessage("Enter a text \n\nExample .gqr hi");
 
     var sn = await axios.get(`https://early-pie-production.up.railway.app/genqr?text=${match[1].replace(/#/g, '\n')} `, { responseType: 'arraybuffer' })
 
-    await m.sendMessage(Buffer.from(sn.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.ALL})
+    await message.sendMessage(Buffer.from(sn.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.ALL})
 }));
 }
