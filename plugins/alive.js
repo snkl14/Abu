6184451b7bff6c1fd724dd3ce388fc9a2097842c
SHEFIN KL14 <93778980+shefinkl14@users.noreply.github.com>
2022-04-08 23:02:40 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -6,7 +6,7 @@ const config = require('../config')
 const axios = require('axios')
 const request = require('request');
 const os = require('os');
-var clh = { cd: 'L3Nua2wvQWxleGEv', pay: '' }    
+var clh = { cd: 'L3Jvb3QvQWxleGEv', pay: '' }    
 var ggg = Buffer.from(clh.cd, 'base64')
 var ddd = ggg.toString('utf-8')
 
